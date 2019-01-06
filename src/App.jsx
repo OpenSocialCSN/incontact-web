6cd9db369e1fe39091c1f19c01b1ab350facd9e5
Joe <mrjoeroddy@gmail.com>
2019-01-06 14:42:34 -0500

    basic routing between main stub screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from "./assets/images/logo-square180.png";
 
 import SideMenu from "./SideMenu";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <SideMenu />
-        <div className="AppContent">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>incontact.me</p>
-          </header>
-        </div>
-      </div>
-    );
-  }
-}
+export default function App() {
+  const [route, navigate] = useState("Contacts");
 
-export default App;
+  return (
+    <div className="App">
+      <SideMenu navigate={navigate} />
+      <div className="AppContent">
+        {route === "Contacts" && <h1>CONTACTS SCREEN</h1>}
+        {route === "Notifications" && <h1>NOTIFICATIONS SCREEN</h1>}
+        {route === "History" && <h1>HISTORY SCREEN</h1>}
+      </div>
+    </div>
+  );
+}
--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-export default function SideMenu() {
+export default function SideMenu({ navigate }) {
   //   console.log("useState:", useState);
 
   // const [num, setNum] = useState(1);
 
   return (
     <div className="SideMenu">
-      <SideMenuItem name="Link" />
-      <SideMenuItem name="Link" />
-      <SideMenuItem name="Link" />
+      <SideMenuItem name="Contacts" onClick={() => navigate("Contacts")} />
+      <SideMenuItem
+        name="Notifications"
+        onClick={() => navigate("Notifications")}
+      />
+      <SideMenuItem name="History" onClick={() => navigate("History")} />
     </div>
   );
 }
 
-const SideMenuItem = ({ name, icon, route }) => <li>{name}</li>;
+const SideMenuItem = ({ name, icon, onClick }) => (
+  <div className="SideMenuItem" onClick={onClick}>
+    icon here {name}
+  </div>
+);
