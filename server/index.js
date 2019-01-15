d062189e7cecce6fe63385eb4bf51c11895dc13d
Joe <mrjoeroddy@gmail.com>
2019-01-15 18:30:44 -0500

    heroku test fix

--- server/index.js
+++ server/index.js
@@ -10,24 +10,28 @@ import establishEndpoints from "./api/endpoints";
 
 const port = process.env.PORT || 5000;
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+async function setupListeners() {
+  const app = express();
 
-establishEndpoints(app);
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-  console.log("Server starting in prod mode");
+  await establishEndpoints(app);
 
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-} else {
-  console.log("Server starting in dv mode");
+  if (process.env.NODE_ENV === "production") {
+    console.log("Server starting in prod mode");
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "../client/build")));
+    // Handle React routing, return all requests to React app
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+  } else {
+    console.log("Server starting in dev mode");
+  }
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+setupListeners();
