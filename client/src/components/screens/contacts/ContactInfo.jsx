8280be01fb62bb0f78e6b279a8817fb7dd7d34b7
Joe <mrjoeroddy@gmail.com>
2019-01-15 14:29:10 -0500

    render basic contact details

--- client/src/components/screens/contacts/ContactInfo.jsx
+++ client/src/components/screens/contacts/ContactInfo.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { MdEdit } from "react-icons/md";
 
 import "./styles/ContactInfo.scss";
+import phone from "../../../assets/images/phone.png";
+import email from "../../../assets/images/email.png";
+import socialImages from "../../../assets/images/social";
 import { Avatar } from "../../reusable";
 
 export default function ContactInfo({ contact = {}, setModal }) {
@@ -41,19 +44,36 @@ export default function ContactInfo({ contact = {}, setModal }) {
             </div>
           </div>
           <div className="column">
-            <div className="card">
+            <div className="ContactDetails card">
               <h2>Contact Details</h2>
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
+              {contact.homeEmail && (
+                <ContactDetailItem
+                  icon={email}
+                  label="HOME"
+                  value={contact.homeEmail}
+                />
+              )}
+              {contact.workEmail && (
+                <ContactDetailItem
+                  icon={email}
+                  label="WORK"
+                  value={contact.workEmail}
+                />
+              )}
+              {contact.homePhone && (
+                <ContactDetailItem
+                  icon={phone}
+                  label="HOME"
+                  value={contact.homePhone}
+                />
+              )}
+              {contact.workPhone && (
+                <ContactDetailItem
+                  icon={phone}
+                  label="WORK"
+                  value={contact.workPhone}
+                />
+              )}
             </div>
           </div>
         </div>
@@ -61,3 +81,15 @@ export default function ContactInfo({ contact = {}, setModal }) {
     </div>
   );
 }
+
+const ContactDetailItem = ({ icon, label, value }) => (
+  <div className="ContactDetailItem">
+    <Icon src={icon} />
+    <span>
+      <div className="ContactDetailItem-label">{label}</div>
+      <div>{value}</div>
+    </span>
+  </div>
+);
+
+const Icon = ({ src }) => <img src={src} alt="" style={{ height: 25 }} />;
