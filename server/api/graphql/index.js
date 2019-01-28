3e656529cee712a05118a4fab29ec2ca9914df9f
Joe <mrjoeroddy@gmail.com>
2019-01-28 16:33:52 -0500

    extract database connection to its own file

--- server/api/graphql/index.js
+++ server/api/graphql/index.js
@@ -1,22 +1,15 @@
 import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
 import { makeExecutableSchema } from "graphql-tools";
-import { MongoClient } from "mongodb";
 import bodyParser from "body-parser";
 
 import { getResolvers } from "./resolvers";
 import { typeDefs } from "./typeDefs";
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASS;
-const dbUrl = `mongodb://${dbUser}:${dbPass}@ds149984.mlab.com:49984/roddy-dev`;
+import dbInstance from "../../db/dbInstance";
 
 async function establishGraphQL(app) {
   let resolvers;
   try {
-    const db = await MongoClient.connect(dbUrl);
+    const db = await dbInstance.getConnection();
     const mongoCollections = {
       Users: db.collection("users"),
       Contacts: db.collection("contacts"),
